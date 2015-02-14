b9dad53139cd66c3dd57768bb343d612ab3abffe
Riley Doyle <rileyd@desktopgenetics.com>
2015-02-14 15:09:14 +0000

    getting things running

--- python/bindings.cpp
+++ python/bindings.cpp
@@ -1,16 +1,37 @@
 
+#include <cstdint>
 
-#include <boost/python.hpp>
+#include <boost/python.hpp> //relative to ENV system import path
+#include <boost/python/list.hpp> // Header file for PyList
+#include <boost/interprocess/file_mapping.hpp>
+#include <boost/interprocess/mapped_region.hpp>
 
-char const* greet()
+#include "../include/suffix_array.h" //means relative path import
+
+bool build(boost::python::list input_files, char* output_file)
 {
-   return "hello, world";
+  using namespace boost::interprocess;
+  //file_mapping fa_file("C:/projects/test/Homo_sapiens.GRCh38.dna.primary_assembly.fa", read_only);
+
+  printf("%d", boost::python::len(input_files));
+
+  /*file_mapping fa_file("C:/projects/test/chr21_p1.fa", read_only);
+
+  mapped_region region(fa_file, read_only, 0, 0);
+  char *begin = (char*)region.get_address();
+  char *end = begin + region.get_size();
+  dna_database dna;
+  parser p;
+  p.add_fasta(&dna, begin, end);
+  */
+  return true;
 }
 
-
-BOOST_PYTHON_MODULE(fred)
+void find_inexact()
 {
+    BOOST_PYTHON_MODULE(suffix_array)
     using namespace boost::python;
-    def("greet", greet);
+    def("build", build);
+    def("find_inexact", find_inexact);
 }
 
