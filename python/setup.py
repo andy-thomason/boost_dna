1f888abab73f1d36bfde92718618f2e424216d40
Riley Doyle <rileyd@desktopgenetics.com>
2015-02-14 14:25:47 +0000

    Set up basic python test

---
+++ python/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='suffix_array',
+    version='0.0.1',
+    packages=['suffix_array'],
+    # TODO C/C++ extensions and dependencies ...
+    install_requires=[
+        'pytest',
+    ],
+)
